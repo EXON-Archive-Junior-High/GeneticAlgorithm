984f6291f9bf26867697394549de9b5e8ebf55cf
EXON <exon.indiv@gmail.com>
2022-09-14 04:35:07 +0000

    Update: 확률 계산

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
 import random as r
 
-arr = [1432, 3249, 1242, 4211]
+d = dict()
+for h in range(1000):
+    s = False
+    result = 0
+    arr = [1432, 3249, 1242, 4211]
+    while (True):
+        result += 1
+        m = 0
+        m1 = 0
 
-result = 0
+        for i in range(len(arr)):
+            if m < str(arr[i]).count('1'):
+                m = str(arr[i]).count('1')
+                m1 = i
 
-while (True):
-    result += 1
-    m = 0
-    m1 = 0
-
-    for i in range(len(arr)):
-        if m < str(arr[i]).count('1'):
-            m = str(arr[i]).count('1')
-            m1 = i
-
-    b = arr[m1]
-    arr = []
-    arr.append(b)
-    for i in range(3):
-        a = list(str(b))
-        a[r.randint(0, 3)] = str(r.randint(1, 9))
-        arr.append(int(''.join(a)))
-    print(arr)
-    
-    for i in range(4):
-        if arr[i] == 1111:
-            print('<*END*>')
-            print(result)
-            exit()
+        b = arr[m1]
+        arr = []
+        arr.append(b)
+        for i in range(3):
+            a = list(str(b))
+            a[r.randint(0, 3)] = str(r.randint(1, 9))
+            arr.append(int(''.join(a)))
+        print(arr)
+        
+        for i in range(4):
+            if arr[i] == 1111:
+                print('<*END*>')
+                print(result)
+                s = True
+        if s:
+            break
+    if result in d.keys():
+        d[result] += 1
+    else:
+        d[result] = 1
+for i in d.keys():
+    print(i, (d[i]/1000))
